b809c940797d5f0e2ee4b41d3120c3777ef40e25
Ryoichi Chiba <99853125+ryoichi-chiba@users.noreply.github.com>
2022-04-03 11:51:51 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict'
+// 1行目に記載している 'use strict' は削除しないでください
+
+// テスト用関数
+function test(actual, expected) {
+    if (JSON.stringify(actual) === JSON.stringify(expected)) {
+      console.log("Yay! Test PASSED.");
+    } else {
+      console.error("Test FAILED. Keep trying!");
+      console.log("    actual: ", actual);
+      console.log("  expected: ", expected);
+      console.trace();
+    }
+}
